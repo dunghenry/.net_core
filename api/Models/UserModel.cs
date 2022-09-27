22966e56e2edda642fd0fa8a7320bbcebde589a8
dunghenry <trandungksnb6930082001>
2022-09-27 16:41:19 +0700

    init

---
+++ api/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [MinLength(length:10)]
+        public string FirstName { get; set; }
+
+    }
+}
